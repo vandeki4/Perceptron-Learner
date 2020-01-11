66b52452608e0a6ae8d87ea97b41f908fa82e6a3
Brendan Vande Kieft <Brendan Vandekieft>
2020-01-11 18:05:09 -0500

    improve training speed + add metrics

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Perceptron-Learner
+
+[Source](learner.py)
+
+## Method
+
+Each class gets its own perceptron, with the goal of distinguishing this class from the others. Documents are encoded as sparse, one-hot vectors.
+The perceptron activation is the sigmoid function, and it is trained with stochastic gradient descent no binary cross-entropy loss as derived in class. In this way, it is similar to
+logistic regression only without a bias term.
+
+Right now, it only does one pass of the train dataset and stops. It is possible to extend this, but one pass was sufficient to show that the model found some signal.
+
+## Results
+[Confusion Matrix](confusion.txt)
+[Per-class accuracy](per-class-acc.txt)
--- learner.py
+++ learner.py
@@ -1,18 +1,24 @@
 import numpy as np
-from sklearn.datasets import fetch_20newsgroups
-
+# One-hot documents are sparse, so use sparse row vectors to make operations faster
+from scipy.sparse import csr_matrix
 from tqdm import tqdm # progress bar
+
+from sklearn.datasets import fetch_20newsgroups
 
 def onehot_document(document, corpus, token_lookup):
     '''Convert a document into a onehot vector using a preexisting dictionary'''
-    onehot = np.zeros(len(corpus))
 
-    for word in document.split():
+    data = []
+    indices = []
+    col_ind = []
+
+    for word in set(document.split()):
         if word in token_lookup:
             idx = token_lookup[word]
-            onehot[idx] = 1.0
+            data.append(1.0)
+            indices.append(idx)
     
-    return onehot
+    return csr_matrix((data, indices, [0, len(indices)]), shape=(1, len(corpus)))
 
 if __name__ == '__main__':
     # Load dataset
@@ -41,7 +47,7 @@ if __name__ == '__main__':
     # I'm using a classifier for each category, so there will be 20 perceptrons.
     weights_for_each_perceptron = np.zeros((20, len(corpus)))
     activation = lambda x: 1.0 / (1.0 + np.exp(-x)) # sigmoid
-    learning_rate = 1e-3
+    learning_rate = 1e-2
 
     # Iterate over each document in the train set, using a progress bar
     for document, class_num in tqdm(
@@ -53,25 +59,29 @@ if __name__ == '__main__':
         for i, weights in enumerate(weights_for_each_perceptron):
             label = 1.0 if (class_num == i) else 0.0
 
-            pred = activation(np.dot(weights, onehot_encoding))
-            grad = (pred - label) * onehot_encoding
-
+            pred = activation(onehot_encoding.dot(weights))
+            
             # Step in the direction of negative gradient
-            weights_for_each_perceptron[i] += (-grad) * learning_rate
+            # This loop exploits the sparse structure of the one_hot array to accelerate the operation.
+            for j, val in zip(onehot_encoding.indices, onehot_encoding.data):
+                grad = (pred - label) * val
+                weights_for_each_perceptron[i, j] += -grad * learning_rate
 
     # Calculate the confusion matrix for this classifier
     confusion = np.zeros((20, 20), dtype=np.int32)
-    for test_document, class_num in zip(test_dataset.data, train_dataset.target):
+    for test_document, class_num in tqdm(
+            zip(test_dataset.data, train_dataset.target),
+            total=len(test_dataset.data)):
         onehot_encoding = onehot_document(test_document, corpus, token_lookup)
 
         # Predict the label by choosing the maximum logit...
-        pred = np.argmax(np.dot(weights_for_each_perceptron, onehot_encoding))
+        pred = np.argmax(onehot_encoding.dot(weights_for_each_perceptron.T))
         confusion[class_num, pred] += 1
     
-    print(confusion)
+    np.savetxt('confusion.txt', confusion / np.sum(confusion, axis=1, keepdims=True), header=' '.join(class_name))
 
-    # Print the per-class accuracy
-    for class_num, pred_counts in enumerate(confusion):
-        acc = np.sum(pred_counts) / pred_counts[class_num]
-
-        print(f'{class_name[class_num]}: {acc}')
+    # Write the per-class accuracy
+    with open('per-class-acc.txt', 'w') as f:
+        for class_num, pred_counts in enumerate(confusion):
+            acc = pred_counts[class_num] / np.sum(pred_counts)
+            f.write(f'{class_name[class_num]}: {acc}\n')
